fee56e234aff467b9f1deb5f5de1b8d9d54d4a90
kawser-Ah <kawser.ah91@gmail.com>
2022-05-23 11:24:08 +0100

    adding a file of multiplication

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 print('Hello github')
 
 # Pull this in the local repository
+import matplotlib.pyplot as plt
+x=[[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]
+y=[[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]
+for i in range(len(x)):
+    plt.figure()
+    plt.plot(x[i],y[i])
+    # Show/save figure as desired.
+    #plt.show()
+# Can show all four figures at once by calling plt.show() here, outside the loop.
+plt.show()
